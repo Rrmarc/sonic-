f0db863775ceae0354b934de36b344f832f32841
Rashell Marcelino <rashellmarcelino@Rashells-MacBook-Air.local>
2019-08-28 10:47:28 -0400

    stoping point and winner alert box

--- main.js
+++ main.js
@@ -21,33 +21,19 @@ let posL2 = 0;
 document.addEventListener("keydown", function(event) {
     // this checks that you pressed the spacebar, if using Mac OS & Chrome
     if (event.keyCode == 39) {
-        //  let img = document.querySelector('img')
-        posL1 += 20;
-        console.log(posL1)
-        player1.style.left = posL1 + "px"
 
-        // if (player1.style.left >= '90px') {
-        //     alert("Player 1 Won!")
-        // }
+        if (posL1 < 600) {
+            posL1 += 20;
+            player1.style.left = posL1 + "px";
+            console.log(player1.style.left);
+        } else { alert('player1 wins') }
 
-    }
-    if (event.keyCode == 100 || event.keyCode == 68) {
-        //  let img = document.querySelector('img')
-        posL2 += 20;
-        console.log(posL2)
-        player2.style.left = posL2 + "px"
-
+    } else if (event.keyCode == 100 || event.keyCode == 68) {
+        if (posL2 < 600) {
+            posL2 += 20;
+            player2.style.left = posL2 + "px";
+            console.log(player2.style.left);
+        } else { alert(' You Win player2!!!'); }
     }
 
 });
-
-document.addEventListener("keyup", function(event) {
-    // this checks that you pressed the spacebar, if using Mac OS & Chrome
-    if (event.keyCode == 100 || event.keyCode == 68) {
-        //  let img = document.querySelector('img')
-        posL2 += 20;
-        console.log(posL2)
-        player2.style.left = posL2 + "px"
-
-    }
-})
